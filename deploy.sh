816121580d65ae1622520106ad4298e6bf1a335d
KMFtcy <kmftangchaoyang@gmail.com>
2020-11-26 22:33:53 +0800

    feat(theme config):move theme config out of theme project

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,22 @@
+# ===
+# === ENV variables
+# ===
+THEMES_CONFIG_FILE_NAME=themes_config.yml
+BACKUP_THEMES_CONFIG_NAME=origin_themes_config.yml
+SELECTED_THEME=next
+
+
+# update themes submodule
+git submodule init
+git submodule update
+# replace themes config
+cp themes/${SELECTED_THEME}/_config.yml ${BACKUP_THEMES_CONFIG_NAME}
+cp ${THEMES_CONFIG_FILE_NAME} themes/${SELECTED_THEME}/_config.yml
+echo "===themes config changed"
+
+# deploy blog
 hexo clean && hexo g && hexo d
+# restore themes config
+cp ${BACKUP_THEMES_CONFIG_NAME} themes/${SELECTED_THEME}/_config.yml
+rm ${BACKUP_THEMES_CONFIG_NAME}
+echo "===themes config restored"
